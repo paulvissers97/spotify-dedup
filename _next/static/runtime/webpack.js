056c04dd9912e5612a9fa7a4007d22ea77a9167e
Jose M. Perez <JMPerez@users.noreply.github.com>
2020-06-02 22:37:24 +0200

    Updates

--- _next/static/runtime/webpack.js
+++ _next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "08acbdb3f651446c43fc";
+/******/ 	var hotCurrentHash = "71f96d55072ad0358df4";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
